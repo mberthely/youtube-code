94bc197548ef5728cf1c328c4403c92b323e6b39
Mario <berthely@gmail.com>
2024-06-12 10:50:17 -0600

    merge

--- yt-angular/src/app/app.routes.ts
+++ yt-angular/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { PersonaComponent } from './features/persona/persona.component';
 import { ClienteComponent } from './features/cliente/cliente.component';
 import { ProductoComponent } from './features/producto/producto.component';
 
 export const routes: Routes = [
-   { path: 'cliente',component: ClienteComponent},
-   { path: 'producto',component: ProductoComponent},
+    {
+        path: 'persona', component: PersonaComponent
+    },
+
+    { path: 'cliente', component: ClienteComponent },
+    { path: 'producto', component: ProductoComponent },
 ];
