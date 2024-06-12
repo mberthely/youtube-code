02df4b7935bbce827b340597586d8015110c5fb8
Mario Berthely <berthely@gmail.com>
2024-06-11 22:03:43 -0600

    component commit

--- yt-angular/src/app/app.routes.ts
+++ yt-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ClienteComponent } from './features/cliente/cliente.component';
+import { ProductoComponent } from './features/producto/producto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: 'cliente',component: ClienteComponent},
+   { path: 'producto',component: ProductoComponent},
+];
