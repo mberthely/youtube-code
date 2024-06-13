c0e65bf27973fc4b27f4f1878b321402d3bb84d1
Mario Berthely <berthely@gmail.com>
2024-06-12 22:58:00 -0600

    arreglando prueba unitaria

--- yt-angular/src/app/app.component.spec.ts
+++ yt-angular/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        RouterModule.forRoot([]),
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +18,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'yt-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('yt-angular');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, yt-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Applicacion de ejemplo YT');
   });
 });
