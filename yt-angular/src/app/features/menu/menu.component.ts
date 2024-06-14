13734f2ecc0bd04fde86c55e414887416475b527
Mario Berthely <berthely@gmail.com>
2024-06-14 01:09:43 -0600

    actualizado el tema de angular material

--- yt-angular/src/app/features/menu/menu.component.ts
+++ yt-angular/src/app/features/menu/menu.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, MatDividerModule, MatIconModule,
+    
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
