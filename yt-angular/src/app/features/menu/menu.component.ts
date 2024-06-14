b558241b1e370681ec320ba9d89972e9b9ed9b4a
Mario Berthely <berthely@gmail.com>
2024-06-14 00:36:15 -0600

    Importacion de mat-button

--- yt-angular/src/app/features/menu/menu.component.ts
+++ yt-angular/src/app/features/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
