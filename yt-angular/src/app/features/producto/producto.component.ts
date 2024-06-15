612f91b776274e59abd3212ba5d82b291813089d
Mario Berthely <berthely@gmail.com>
2024-06-14 23:32:48 -0600

    Utilizando tabla de angular material

--- yt-angular/src/app/features/producto/producto.component.ts
+++ yt-angular/src/app/features/producto/producto.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Producto {
+  nombre: string;
+  precio: number;
+}
+
+const ELEMENT_DATA: Producto[] = [
+  { nombre: "Prueba", precio: 10 },
+  { nombre: "Prueba 2", precio: 20 },
+];
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule,
+    MatButtonModule
+  ],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.scss'
 })
-export class ProductoComponent {
 
+export class ProductoComponent {
+  displayedColumns: string[] = ['nombre', 'precio', 'acciones'];
+  dataSource = ELEMENT_DATA;
 }
