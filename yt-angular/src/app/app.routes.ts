6b27ea83f4681c342066670fb5d4ced948d10be8
Mario Berthely <berthely@gmail.com>
2024-06-16 22:04:55 -0700

    delete persona

--- yt-angular/src/app/app.routes.ts
+++ yt-angular/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-import { PersonaComponent } from './features/persona/persona.component';
 import { ClienteComponent } from './features/cliente/cliente.component';
 import { ProductoComponent } from './features/producto/producto.component';
 
 export const routes: Routes = [
-    {
-        path: 'persona', component: PersonaComponent
-    },
-
     { path: 'cliente', component: ClienteComponent },
     { path: 'producto', component: ProductoComponent },
 ];
