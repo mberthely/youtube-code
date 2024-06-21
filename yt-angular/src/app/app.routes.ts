d15c1f10ed7fd246c34cbb555764e0461de34c59
Mario Berthely <berthely@gmail.com>
2024-06-20 21:46:56 -0700

    ejemplo router link y navegacion

--- yt-angular/src/app/app.routes.ts
+++ yt-angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ClienteComponent } from './features/cliente/cliente.component';
 import { ProductoComponent } from './features/producto/producto.component';
+import { AcercaComponent } from './features/acerca/acerca.component';
 
 export const routes: Routes = [
     { path: 'cliente', component: ClienteComponent },
     { path: 'producto', component: ProductoComponent },
+    { path: 'acerca', component: AcercaComponent },
 ];
