fa965766d182bf172df1e5edce4ce2ff5c9cdc4a
Mario Berthely <berthely@gmail.com>
2024-08-04 00:08:00 -0700

    ejemplo defer

--- yt-angular/src/app/app.routes.ts
+++ yt-angular/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
-import { ClienteComponent } from './features/cliente/cliente.component';
-import { ProductoComponent } from './features/producto/producto.component';
 import { AcercaComponent } from './features/acerca/acerca.component';
 import { CalculadoraComponent } from './features/calculadora/calculadora.component';
+import { ClienteComponent } from './features/cliente/cliente.component';
+import { DeferExampleComponent } from './features/defer-example/defer-example.component';
+import { ProductoComponent } from './features/producto/producto.component';
 
 export const routes: Routes = [
-    { path: 'cliente', component: ClienteComponent },
-    { path: 'producto', component: ProductoComponent },
-    { path: 'acerca', component: AcercaComponent },
-    { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: 'acerca', component: AcercaComponent },
+  { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'defer', component: DeferExampleComponent },
 ];
---
+++ yt-angular/src/app/features/defer-example/defer-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './defer-example.component.html',
+  styleUrl: './defer-example.component.scss'
+})
+export class DeferExampleComponent {
+
+}
